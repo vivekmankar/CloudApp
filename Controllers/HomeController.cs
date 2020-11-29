8cb760bd4fdfe5b165447db524ab4105004079e4
Vuchula <70232007+Vuchula@users.noreply.github.com>
2020-11-29 09:49:51 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CloudApp.Models;
+
+namespace CloudApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		public IActionResult AboutUs()
+		{
+			return View("AboutUS");
+		}
+
+		public IActionResult Crashstats()
+		{
+			return View("Crashstats");
+		}
+
+		public IActionResult Visualize()
+		{
+			return View("Visualize");
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
