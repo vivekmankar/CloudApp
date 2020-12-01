a726cf8086a8260cb4cd762e94f32a8c66cec91f
vivekmankar <62715388+vivekmankar@users.noreply.github.com>
2020-12-01 00:15:29 -0500

    Add files via upload

---
+++ Models/CrashList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudApp.Models
+{
+    public class CrashList
+    {
+        public List<Crashes> crashList { get; set; }
+        
+    }
+}
